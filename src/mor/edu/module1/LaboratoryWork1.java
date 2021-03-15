d2fe99214abe06c665f656420fcf172b24b94f89
AntoninaMoroz <moroztonya8@ukr.net>
2021-03-15 20:06:57 +0200

    lr

--- src/mor/edu/module1/LaboratoryWork1.java
+++ src/mor/edu/module1/LaboratoryWork1.java
@@ -20,34 +20,163 @@ public class LaboratoryWork1 {
 
         String text = new String(Files.readAllBytes(Paths.get("/Users/User/Desktop/Java/harry.txt")));
 
+        //Take  the array of distinct words from Harry Potter .
+
         String[] lines = text.split("\\n");
 
-        System.out.println(lines.contains("Harry"));
+        int counter = 0;
 
-        String string = text;
-        String[] parts = string.split(" C");
-
-        for (int i = 0; i < parts.length; i++) {
-            String[] word = parts[i].split(" ");
-
-            if (i > 0) {
-                System.out.println("C" + word[0]);
-            } else {
-                for (int j = 0; j < word.length; j++) {
-
-                    if (word[j].startsWith("c") || word[j].startsWith("C"))
-                        System.out.println(word[j]);
-
-                    text = text.replaceAll("[^A-Za-z ']", "");
-
-                    String[] words = text.split(" ");
-
-                    String[] hash = words.hashCode();
-
-
-                }
-
+        for (int i = 0; i < lines.length; i++) {
+            if (lines[i].contains("Harry")) {
+                counter++;
             }
+        }
+        System.out.println(counter);
+
+        //Take  the array of distinct words from Harry Potter .
+
+        text = text.replaceAll("[^A-Za-z ']", "");
+
+        String[] words = text.split(" ");
+
+        String stringOfDistincts = "";
+
+        for (int i = 0; i < words.length; i++) {
+
+            if (!stringOfDistincts.contains(words[i])) {
+                stringOfDistincts += words[i] + " ";
+            }
+        }
+        String[] distincts = stringOfDistincts.split(" ");
+
+     // How many distinct words begin from the letter "C".
+
+        int wordC = 0;
+
+        for (int i = 0; i < distincts.length; i++) {
+            if (distincts[i].startsWith("C")) {
+                wordC++;
+            }
+        }
+        System.out.println(wordC);
+
+        //Create an array of hashes.
+
+        int[] hashes = new int[distincts.length];
+        for (int i = 0; i < distincts.length; i++) {
+            hashes[i] = distincts[i].hashCode();
+        }
+        for (int i = 0; i < 100; i++) {
+
+            System.out.println(hashes[i]);
         }
     }
 }
+
+/* result:
+1084
+80
+69496448
+-1898553908
+96727
+114801
+-409322317
+80218181
+1456998445
+78406
+83025
+65964
+85918
+72447192
+2501
+77646
+-615834066
+3543
+-1034364087
+3149094
+-1896109122
+66300266
+3645825
+106940924
+113643
+3558823
+3558952
+471465562
+-1039745817
+110323434
+119839
+3616054
+3362829
+2605640
+3314326
+-991808881
+115166588
+-1289163687
+636733255
+-330473854
+-1881997444
+1827208126
+-232973818
+3273964
+-1331996356
+3208383
+3649734
+3541575
+1422799105
+117481
+246043532
+3143230
+-1367775363
+1349413851
+113101341
+3343637
+-1323535496
+2333
+97536
+93614293
+107866
+-1224458312
+3377247
+2035477926
+3195240
+102742843
+853982760
+-1386154518
+103051
+-1049482315
+110777640
+111593474
+-1413853096
+3046006
+-836041066
+113840
+109642094
+3676
+103189
+3560141
+1025237382
+3423444
+-1253087691
+-1278142878
+-895471450
+425173953
+-1910986747
+109548807
+114066
+2057920365
+110327130
+-1259490430
+110327396
+97436152
+97740
+-327707013
+401590963
+-795125073
+97921
+2997191
+-906277200
+201724889
+
+Process finished with exit code 0
+
+ */
